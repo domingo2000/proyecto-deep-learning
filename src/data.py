fix(experiments/train): train input and labels where not shifted so the output produced was not future tokens, also added padding masking for src and tgt and attn_mask for future attention as in language-translation

src/data.py
@@ -4,7 +4,9 @@ from torch.utils.data import Dataset
 
 
 class SCANDataset(Dataset):
-    def __init__(self, path, transform=None, target_transform=None):
+    def __init__(
+        self, path, transform=None, target_transform=None, label_transform=None
+    ):
         with open(path) as file:
             instances = file.readlines()
             instances = [instance.replace("IN: ", "") for instance in instances]
@@ -16,6 +18,7 @@ class SCANDataset(Dataset):
         self.instances = instances
         self.transform = transform
         self.target_transform = target_transform
+        self.label_transform = label_transform
 
     def __len__(self):
         return len(self.instances)
@@ -24,8 +27,11 @@ class SCANDataset(Dataset):
         instance = self.instances[idx]
         x_i = instance[0]
         y_i = instance[1]
+        if self.label_transform:
+            y_label = self.label_transform(y_i)
         if self.transform:
             x_i = self.transform(x_i)
         if self.target_transform:
             y_i = self.target_transform(y_i)
-        return x_i, y_i
+
+        return x_i, y_i, y_label

src/main.py
@@ -27,9 +27,14 @@ output_tokenizer = OutputTokenizer()
 train_path = os.path.join("src", "tasks_toy.txt")
 train_dataset = SCANDataset(
     path=train_path,
-    transform=lambda x: torch.tensor(input_tokenizer.encode(preprocessor.transform(x))),
+    transform=lambda x: torch.tensor(
+        input_tokenizer.encode(preprocessor.input_transform(x)),
+    ),
     target_transform=lambda x: torch.tensor(
-        output_tokenizer.encode(preprocessor.transform(x))
+        output_tokenizer.encode(preprocessor.output_transform(x, sos=True))
+    ),
+    label_transform=lambda x: torch.tensor(
+        output_tokenizer.encode(preprocessor.output_transform(x, eos=True))
     ),
 )
 
@@ -37,7 +42,7 @@ train_dataset = SCANDataset(
 # Dataloaders
 train_dataloader = DataLoader(train_dataset, batch_size=P.BATCH_SIZE)
 
-x, y = next(iter(train_dataloader))
+x, y, y_label = next(iter(train_dataloader))
 
 print("")
 print(x)
@@ -71,21 +76,16 @@ def tokenization_sequence_to_string(tokenized_sequence, tokenizer=input_tokenize
 # Generator Function
 def generate(x_i):
     eos_token = output_tokenizer.encode("<EOS>")[0]
-
-    target_sequence = ["<PAD>" for i in range(P.CONTEXT_LENGTH - 1)]
-    target_sequence.insert(0, "<SOS>")
-    target_sequence = " ".join(target_sequence)
-    target_sequence = output_tokenizer.encode(target_sequence)
-
-    target_sequence = torch.tensor(target_sequence, device=device)
-
+    target_sequence = torch.tensor(
+        output_tokenizer.encode(preprocessor.output_transform("", sos=True, eos=False)),
+        device=device,
+    )
     current_target_idx = 0
     while eos_token not in target_sequence:
         y_pred_logits = model.forward(x_i, target_sequence)
         y_pred = torch.multinomial(y_pred_logits, num_samples=1)
 
         next_token = y_pred[current_target_idx].item()
-
         current_target_idx += 1
 
         if current_target_idx == P.CONTEXT_LENGTH:
@@ -99,17 +99,18 @@ def generate(x_i):
 for epoch in range(P.EPOCHS):
     epoch_loss = 0
     for batch_n, batch in enumerate(train_dataloader):
-        x, y = batch
+        x, y, y_label = batch
         x = x.to(device)
         y = y.to(device)
+        y_label = y_label.to(device)
 
         logits = model(x, y)
 
         B, T, C = logits.shape
         logits = logits.view(B * T, C)
-        y = y.view(B * T)
+        y_label = y_label.view(B * T)
 
-        loss = criterion(logits, y)
+        loss = criterion(logits, y_label)
 
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
@@ -123,10 +124,8 @@ for epoch in range(P.EPOCHS):
     if epoch % 100 == 0:
         print(f"| epoch: {epoch} | loss: {epoch_loss:.2f} |")
         x_test = train_dataset.transform("jump").to(device)
-        # x_test = x_test.reshape((1, *x_test.shape))
         x_target = train_dataset.target_transform("I_JUMP").to(device)
-        # x_target = x_target.reshape((1, *x_target.shape))
-        logits = model(x_test, x_target)  # generate(x_test)
+        logits = model(x_test, x_target)
 
         y_test = torch.multinomial(logits, num_samples=1)
         y_test = y_test.squeeze()
@@ -134,12 +133,20 @@ for epoch in range(P.EPOCHS):
             f"IN: {tokenization_sequence_to_string(x_test, tokenizer=input_tokenizer)}: OUT_PRED: {tokenization_sequence_to_string(y_test, tokenizer=output_tokenizer)}"
         )
 
-        # Jump Twice
+        # Jump Generated
+        x_test = train_dataset.transform("jump").to(device)
+        y_test = generate(x_test)
+        print(
+            f"IN: {tokenization_sequence_to_string(x_test, tokenizer=input_tokenizer)}: OUT_PRED: {tokenization_sequence_to_string(y_test, tokenizer=output_tokenizer)}"
+        )
+
+        # Jump twice Generated
+
+        # Without generation, passing all the input as training set
+        print(f"| epoch: {epoch} | loss: {epoch_loss:.2f} |")
         x_test = train_dataset.transform("jump twice").to(device)
-        # x_test = x_test.reshape((1, *x_test.shape))
         x_target = train_dataset.target_transform("I_JUMP I_JUMP").to(device)
-        # x_target = x_target.reshape((1, *x_target.shape))
-        logits = model(x_test, x_target)  # generate(x_test)
+        logits = model(x_test, x_target)
 
         y_test = torch.multinomial(logits, num_samples=1)
         y_test = y_test.squeeze()
@@ -147,39 +154,9 @@ for epoch in range(P.EPOCHS):
             f"IN: {tokenization_sequence_to_string(x_test, tokenizer=input_tokenizer)}: OUT_PRED: {tokenization_sequence_to_string(y_test, tokenizer=output_tokenizer)}"
         )
 
-
-# model.train()  # Turn on the train mode
-# total_loss = 0.0
-# start_time = time.time()
-# src_mask = model.generate_square_subsequent_mask(bptt).to(device)
-# for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
-#     data, targets = get_batch(train_data, i)
-#     optimizer.zero_grad()
-#     if data.size(0) != bptt:
-#         src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
-#     output = model(data, src_mask)
-#     loss = criterion(output.view(-1, ntokens), targets)
-#     loss.backward()
-#     torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-#     optimizer.step()
-
-#     total_loss += loss.item()
-#     log_interval = 200
-#     if batch % log_interval == 0 and batch > 0:
-#         cur_loss = total_loss / log_interval
-#         elapsed = time.time() - start_time
-#         print(
-#             "| epoch {:3d} | {:5d}/{:5d} batches | "
-#             "lr {:02.2f} | ms/batch {:5.2f} | "
-#             "loss {:5.2f} | ppl {:8.2f}".format(
-#                 epoch,
-#                 batch,
-#                 len(train_data) // bptt,
-#                 scheduler.get_last_lr()[0],
-#                 elapsed * 1000 / log_interval,
-#                 cur_loss,
-#                 math.exp(cur_loss),
-#             )
-#         )
-#         total_loss = 0
-#         start_time = time.time()
+        # With generation, passing only <SOS>
+        x_test = train_dataset.transform("jump twice").to(device)
+        y_test = generate(x_test)
+        print(
+            f"IN: {tokenization_sequence_to_string(x_test, tokenizer=input_tokenizer)}: OUT_PRED: {tokenization_sequence_to_string(y_test, tokenizer=output_tokenizer)}"
+        )

src/transfomer.py
@@ -24,6 +24,7 @@ class TransformerModel(nn.Module):
             batch_first=True,  # (BATCH, SEQUENCE_LENGTH, FEATURE_DIM)
         )
 
+        # Boolean mask for masked self attention on decoder
         self.mask = nn.Transformer.generate_square_subsequent_mask(
             sz=P.CONTEXT_LENGTH, device=device
         )
@@ -41,6 +42,13 @@ class TransformerModel(nn.Module):
         self.linear_feed_forward.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src, tgt):
+        # Create mask for all the padding in the ouput
+        output_pad_token = 8
+        tgt_key_padding_mask = tgt == output_pad_token
+
+        input_pad_token = 15
+        src_key_padding_mask = src == input_pad_token
+
         src_embedding = self.input_embedding(src)
         src_pos_embedding = self.input_position_embedding(src)
         src = src_embedding + src_pos_embedding
@@ -48,7 +56,13 @@ class TransformerModel(nn.Module):
         tgt_pos_embedding = self.target_position_embedding(tgt)
         tgt = tgt_embedding + tgt_pos_embedding
 
-        output = self.transformer(src, tgt, tgt_mask=self.mask)
+        output = self.transformer(
+            src,
+            tgt,
+            tgt_mask=self.mask,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
+        )
         output = self.linear_feed_forward(output)
         output = self.softmax(output)
 